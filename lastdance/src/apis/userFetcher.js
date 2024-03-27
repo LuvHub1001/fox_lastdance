33ee74872a7a948c9709627ab0f95d01a8ff3fa8
dy7791 <dy7791@naver.com>
2024-03-27 10:08:59 +0900

    working:: backoff

--- lastdance/src/apis/userFetcher.js
+++ lastdance/src/apis/userFetcher.js
@@ -1,7 +1,15 @@
-import axios from "axios";
+import axios, { AxiosError, HttpStatusCode, isAxiosError } from "axios";
+
+const RETRY_TIME_COUNT = 500;
+const AXIOS_TIMEOUT = 3000;
+
+// 최대 3번 재시도
+const retryMaxCount = 3;
+const retryCount = 0;
 
 const instanceAxios = axios.create({
   baseURL: "https://fakestoreapi.com/",
+  timeout: AXIOS_TIMEOUT,
   headers: {
     "Content-Type": "application/json",
   },
@@ -15,16 +23,61 @@ instanceAxios.interceptors.request.use(
     return req;
   },
   (err) => {
+    if (isAxiosError(err)) {
+      if (err.status === HttpStatusCode.NotFound) {
+        alert(
+          `Cannot Find Endpoint :: ${err.config?.baseURL} ${err.config.url}`,
+        );
+      } else {
+        throw err;
+      }
+    }
+
     return Promise.reject(err);
   },
 );
 
 instanceAxios.interceptors.response.use(
   (res) => {
+    retryCount = 3;
     return res;
   },
   (err) => {
-    return Promise.reject(err);
+    if (isAxiosError(err) && err.response) {
+      switch (err.response.status) {
+        case HttpStatusCode.BadRequest:
+          console.error(`Bad Request :: ${err.config.data}`);
+          break;
+
+        case HttpStatusCode.NotFound:
+          while (retryCount++ < retryMaxCount) {
+            backoffRequest(RETRY_TIME_COUNT * retryCount, err.config);
+          }
+          break;
+
+        case HttpStatusCode.Unauthorized:
+          console.error(`Unauthorized :: ${err.config.headers}`);
+          break;
+
+        case HttpStatusCode.Forbidden:
+          console.error(`Forbidden :: ${err.config.headers}`);
+          break;
+
+        case HttpStatusCode.BadGateway:
+          while (retryCount++ < retryMaxCount) {
+            backoffRequest(RETRY_TIME_COUNT * retryCount, err.config);
+          }
+          break;
+        default:
+          console.error(
+            `Error :: status : ${err.status} message: ${err.message}`,
+          );
+
+          break;
+      }
+    } else {
+      throw err;
+    }
   },
 );
 
@@ -42,4 +95,10 @@ export const patch = async (url, data, config) => {
 
 export const del = async (url, config) => {
   return await instanceAxios.delete(url, config);
+};
+
+const backoffRequest = async (times, config) => {
+  setTimeout(async () => {
+    await this.fetcher.request(config);
+  }, times);
 };
